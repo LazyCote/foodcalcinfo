74af70538ea64e3d8ac1c3c3a60329930cffadb3
s2d1ent <vornfrost@gmail.com>
2021-03-09 18:59:52 +0300

    v0.0.2

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ var inputScripts = [
 
 
 $("document").ready(()=>{
-  regAuthForm();
+    regAuthForm();
 })
 
 wiw.addEventListener("scroll",(event)=>{
@@ -22,7 +22,7 @@ wiw.addEventListener("scroll",(event)=>{
     $(".header").css("position","");
   }
 })
-  for (var i=0;i<scripts.length;i++) {
+for (var i=0;i<scripts.length;i++) {
     if (scripts[i].getAttribute("src").includes("*")==true) {
       scripts[i].setAttribute("src",scripts[i].getAttribute("src").replace("*","https://lazycote.github.io"));
     }
@@ -52,7 +52,7 @@ for (var i=0;i<a.length;i++) {
     }
   }
 function regAuthForm() {
-  for (var i=1;i<31;i++) {
+  for (var i=1;i<32;i++) {
     let option = doc.createElement("option");
     option.setAttribute("value",i);
     option.innerHTML=i;
@@ -96,5 +96,55 @@ $("#reg").bind("click",()=>{
     $(".registerForm").css("display","flex");
 });
 $(".browser").bind("click",()=>{
-  
+  $("#auth").click();
 });
+function calc () {
+let rad=$(".labelRad"),
+    radArr = Array.from(rad);
+var lastI,
+    newR,
+    valTarget;
+
+function newChange(th) {
+  th.style.fontSize="14px";
+  th.style.fontWeight="500";
+  th.style.color="#56CCF2";
+}
+function lastChange(th) {
+  rad[lastI].style.fontSize="12px";
+  rad[lastI].style.fontWeight="400";
+  rad[lastI].style.color="black";
+}
+rad.bind("click",(event)=>{
+      let target = event.target,
+          targetJQ = $(event.target),
+          index =radArr.indexOf(target),
+          radio = rad[index];
+      newChange(radio);
+      valTarget =targetJQ.attr("value");
+      if (newR==null || newR==undefined) {
+        lastI=index;
+      }
+      newR=index;
+      if (lastI!=newR) {
+        lastChange(rad[lastI]);
+        lastI=newR;
+      }
+});
+$(".btn-test").bind("click",()=>{
+  let weight=parseInt($("#weightF").val()),
+      height=parseInt($("#heightF").val()),
+      old=parseInt($("#oldF").val()),
+      genderF=parseInt($("#genderf").val());
+      valTarget=parseInt(valTarget);
+  let gender;
+  switch (genderF) {
+    case 1:gender=5; break;
+    case 0:gender=-161; break;
+  }
+  let dci = weight*10 + height*6.25-old*5+gender;
+      dci = dci *valTarget
+  $("#normalCalc").val(dci);
+})
+}
+calc();
