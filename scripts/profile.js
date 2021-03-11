bac4b9c77561679d92b9649a4bbaab516e2a0230
s2d1ent <vornfrost@gmail.com>
2021-03-11 23:26:52 +0300

    v0.0.6

--- scripts/profile.js
+++ scripts/profile.js
@@ -13,6 +13,7 @@ function content() {
 		content1080Open();
 
 }
+// выдвижной сайдбар
 $(".left__burger").bind("click",()=>{
 	burgerCountI=burgerCount%2;
 	burgerCount++;
@@ -44,29 +45,87 @@ function left__profileInfoClose() {
 	$(".left__profileInfo-text").css("display","none");
 	$(".left__points-title ").css("display","none");
 }
+
 // event at click points of menu
-$(".chatAll").bind("click",chatAll);
-$(".chatRegion").bind("click",chatRegion);
-$(".recipe").bind("click",recipe);
-$(".calories").bind("click",calories);
-$(".foodTheree").bind("click",foodTheree);
-
-function chatAll() {
-// установить соединение с сервером
-
-//видоизменить блок container__down_block-header
-
-//вывести информацию в блок
+// индекс меню
+function functionName(th) {
+	th.bind("click",(event)=>{
+		let target = event.target;
+		let arrayBlock=Array.from(th),
+				index=arrayBlock.indexOf(target);
+				console.log(index)
+		checkMenuPoints(index);
+	});
 }
-function chatRegion() {
+// Блоки меню
+functionName($(".right__container_block-header"));
+functionName($(".block__header-title"));
+functionName($(".block__header-img"));
+//
+function checkMenuPoints(th) {
+	switch (th) {
+		case 0:
+		closeWindowMenu("chatAll");
+			break;
+		case 1:
+		closeWindowMenu("chatRegion");
+			break;
+		case 2:
+		closeWindowMenu("recipe");
+			break;
+		case 3:
+		closeWindowMenu(th)
+			break;
+		case 4:
+		closeWindowMenu(th)
+			break;
+		default:
+	}
+}
+function closeWindowMenu(th) {
+$(".chatRegion").css("display","none");
+$(".chatAll").css("display","none");
+$(".recipe").css("display","none");
+
+$(".right_container_down-block").css("height","100%");
+$(".container__down_block-header").css("display","none");
+$("."+th+"__container-chat").css("height",parseInt($(".right_container_down-block").css("height"))-100);
+$("."+th+"").css("display","flex");
+}
+function connectDataBase(th) {
 
 }
-function recipe() {
-
+// отправка сообщения
+function toSend() {
+	$("#chattAllBtn").bind("click",()=>{
+	var user = "s2d1ent",
+			message=$(".chattAllMessage").val();
+			if (message=="") {
+				return;
+			}
+			if (user==$(".messageChatAllBlock-name").val()) {
+				$(".messageChatAllBlock").classList.add(".myMessageM");
+			}
+	var text = `<div class="messageChatAllBlock" user-data="">
+		<span class="messageChatAllBlock-name">`+user+`</span>
+		<span class="messageChatAllBlock-mess">`+message+`</span>
+	</div>`;
+	$(".chatAll__container-chat").append(text);
+	});
+	$("#chattRegionBtn").bind("click",()=>{
+	var user = "s2d1ent",
+			message=$(".chattRegionMessage").val();
+			if (message=="") {
+				return;
+			}
+			if (user==$(".messageChatRegionBlock-name").val()) {
+				$(".messageChatRegionBlock").classList.add(".myMessageM");
+			}
+	var text = `<div class="messageChatRegionBlock" user-data="">
+		<span class="messageChatRegionBlock-name">`+user+`</span>
+		<span class="messageChatRegionBlock-mess">`+message+`</span>
+	</div>`;
+	$(".chatRegion__container-chat").append(text);
+	});
 }
-function calories() {
-
-}
-function foodTheree() {
-
-}
+toSend();
